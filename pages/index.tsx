ce6aba9c1bfe1d8c4b88f7a21b2421f7b105bcc4
goncalosvg <medeiror2005@gmail.com>
2021-08-12 01:41:25 +0100

    Created hero section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
+import Navigation from '../components/Navigation/Navigation';
+import Topics from '../components/Topics/Topics';
+
+import styles from '../styles/pagestyles/index.module.css';
+
 function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <main>
+      <Navigation />
+
+      <div id={styles.hero}>
+        <div className="wrapper">
+
+          <h1>Have a question?<br />We have the <span className="gradient-text">answer</span></h1>
+
+        </div>
+        <Topics />
+      </div>
+
+    </main>
   )
 }
 
